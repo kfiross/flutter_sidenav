9aeb9754e428ae30dea04704022ee5c8ec28f8c6
kfiross <kfir25812@gmail.com>
2021-05-03 16:46:20 +0300

    null-safety migration

--- lib/flutter_sidenav.dart
+++ lib/flutter_sidenav.dart
@@ -3,9 +3,9 @@ library flutter_sidenav;
 import 'package:flutter/material.dart';
 
 class SideNavItem {
-  final String name;
-  final IconData iconData;
-  final Widget page;
+  final String? name;
+  final IconData? iconData;
+  final Widget? page;
 
   const SideNavItem({
     this.name,
@@ -22,17 +22,17 @@ class SideBar extends StatefulWidget {
   //final Widget currentItem;
 
   final List<SideNavItem> items;
-  final Function show;
+  final Function? show;
 
   // final int selectedIndex;
-  final Color selectedColor;
+  final Color? selectedColor;
   final bool alwaysOpened;
 
-  final PageController pageController;
+  final PageController? pageController;
 
   SideBar({
     this.backgroundColor = Colors.blueGrey,
-    @required this.items,
+    required this.items,
     this.sideBarCollapsedWidth = 60,
     this.sideBarWidth = 180,
     this.show,
@@ -48,7 +48,7 @@ class SideBar extends StatefulWidget {
 class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin {
   int currentIndex = 0;
 
-  List<Widget> pages = [];
+  List<Widget?> pages = [];
 
   List<int> history = [];
 
@@ -56,9 +56,9 @@ class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin {
   void initState() {
     pages = widget.items.map((item) => item.page).toList();
 
-    widget.pageController.addListener(() {
+    widget.pageController!.addListener(() {
       // final Widget currentItem =  widget.items[currentItemIndex].page;
-      currentIndex = widget.pageController.page.toInt();
+      currentIndex = widget.pageController!.page!.toInt();
     });
 
     super.initState();
@@ -82,28 +82,28 @@ class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin {
             onItemTap: widget.show),
         Expanded(
             child: PageView(
-              physics: NeverScrollableScrollPhysics(),
-              //child: pages[currentIndex]),
-              children: pages,
-              controller: widget.pageController,
-            ))
+          physics: NeverScrollableScrollPhysics(),
+          //child: pages[currentIndex]),
+          children: pages as List<Widget>,
+          controller: widget.pageController,
+        ))
       ]),
     );
   }
 }
 
 class SideNavMenu extends StatefulWidget {
-  final List<SideNavItem> items;
-  final Color selectedColor;
-  final Function onItemTap;
-  final bool alwaysOpen;
+  final List<SideNavItem>? items;
+  final Color? selectedColor;
+  final Function? onItemTap;
+  final bool? alwaysOpen;
   final double sideBarWidth;
   final double sideBarCollapsedWidth;
-  final Color backgroundColor;
-  final PageController pageController;
+  final Color? backgroundColor;
+  final PageController? pageController;
 
   const SideNavMenu({
-    Key key,
+    Key? key,
     this.items,
     this.selectedColor,
     this.onItemTap,
@@ -125,8 +125,8 @@ class _SideNavMenuState extends State<SideNavMenu>
   bool _first = true;
   int _selectedIndex = 0;
 
-  AnimationController _controller;
-  Animation<double> _animation;
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void dispose() {
@@ -136,9 +136,8 @@ class _SideNavMenuState extends State<SideNavMenu>
 
   @override
   void initState() {
-
     super.initState();
-    if (widget.alwaysOpen) {
+    if (widget.alwaysOpen!) {
       isCollapsed = false;
     }
 
@@ -163,15 +162,13 @@ class _SideNavMenuState extends State<SideNavMenu>
       }
     });
 
-
-    widget.pageController.addListener(() {
-      if(this.mounted) {
+    widget.pageController!.addListener(() {
+      if (this.mounted) {
         setState(() {
-          _selectedIndex = widget.pageController.page.toInt();
+          _selectedIndex = widget.pageController!.page!.toInt();
         });
       }
     });
-
   }
 
   @override
@@ -183,7 +180,7 @@ class _SideNavMenuState extends State<SideNavMenu>
             .animate(_controller);
 
     return AnimatedContainer(
-      duration: _controller.duration,
+      duration: _controller.duration!,
       color: widget.backgroundColor,
       width: isCollapsed ? widget.sideBarCollapsedWidth : widget.sideBarWidth,
       child: Container(
@@ -197,39 +194,39 @@ class _SideNavMenuState extends State<SideNavMenu>
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: widget.items
+                children: widget.items!
                     .asMap()
                     .map((index, item) {
-                  final view = NavItemTile(
-                    isCollapsed: showText ? true : isCollapsed,
-                    // hoverColor: Colors.blue,
-                    selectedColor: _selectedIndex != index
-                        ? null
-                        : widget.selectedColor,
-                    title: Text(
-                      item.name,
-                      style: TextStyle(
-                        color:
-                        _selectedIndex == index ? Colors.white : null,
-                      ),
-                    ),
-                    icon: item.iconData == null
-                        ? null
-                        : Icon(
-                      item.iconData,
-                      color: _selectedIndex == index
-                          ? Colors.white
-                          : null,
-                    ),
-                    onPressed: () => widget.onItemTap(index),
-                  );
-                  return MapEntry<int, Widget>(index, view);
-                })
+                      final view = NavItemTile(
+                        isCollapsed: showText ? true : isCollapsed,
+                        // hoverColor: Colors.blue,
+                        selectedColor: _selectedIndex != index
+                            ? null
+                            : widget.selectedColor,
+                        title: Text(
+                          item.name!,
+                          style: TextStyle(
+                            color:
+                                _selectedIndex == index ? Colors.white : null,
+                          ),
+                        ),
+                        icon: item.iconData == null
+                            ? null
+                            : Icon(
+                                item.iconData,
+                                color: _selectedIndex == index
+                                    ? Colors.white
+                                    : null,
+                              ),
+                        onPressed: () => widget.onItemTap!(index),
+                      );
+                      return MapEntry<int, Widget>(index, view);
+                    })
                     .values
                     .toList(),
               ),
             ),
-            if (!widget.alwaysOpen) ...[
+            if (!widget.alwaysOpen!) ...[
               Container(
                 margin: EdgeInsetsDirectional.only(end: isCollapsed ? 0 : 12),
                 alignment: isCollapsed
@@ -247,8 +244,8 @@ class _SideNavMenuState extends State<SideNavMenu>
                               end: _first
                                   ? 0
                                   : isCollapsed
-                                  ? 8
-                                  : 8,
+                                      ? 8
+                                      : 8,
                             ),
                             child: Icon(
                                 _first
@@ -281,20 +278,20 @@ class _SideNavMenuState extends State<SideNavMenu>
 }
 
 class NavItemTile extends StatelessWidget {
-  final Color hoverColor;
+  final Color? hoverColor;
   final Widget title;
-  final Widget icon;
+  final Widget? icon;
   final bool isCollapsed;
-  final Function onPressed;
-  final Color selectedColor;
+  final Function? onPressed;
+  final Color? selectedColor;
 
   NavItemTile(
-      {@required this.isCollapsed,
-        @required this.title,
-        @required this.icon,
-        this.onPressed,
-        this.hoverColor,
-        this.selectedColor});
+      {required this.isCollapsed,
+      required this.title,
+      required this.icon,
+      this.onPressed,
+      this.hoverColor,
+      this.selectedColor});
 
   @override
   Widget build(BuildContext context) {
@@ -304,7 +301,7 @@ class NavItemTile extends StatelessWidget {
         style: TextButton.styleFrom(
           padding: EdgeInsets.all(0),
         ),
-        onPressed: onPressed,
+        onPressed: onPressed as void Function()?,
         child: ListTile(
           selected: selectedColor != null,
           selectedTileColor: selectedColor,
@@ -319,19 +316,21 @@ class NavItemTile extends StatelessWidget {
 class SideBarScaffold extends StatefulWidget {
   final List<SideNavItem> items;
   final Color backgroundColor;
-  final Color selectedColor;
+  final Color? selectedColor;
   final double sideBarWidth;
-  final bool alwaysOpened;
-  final bool showSelectedBold;
+  final bool? alwaysOpened;
+  final bool? showSelectedBold;
+
   // final PreferredSizeWidget appBar;
 
   const SideBarScaffold({
-    Key key,
+    Key? key,
     this.backgroundColor = Colors.blueGrey,
     this.sideBarWidth = 190,
-    @required this.items,
+    required this.items,
     this.selectedColor,
-    this.alwaysOpened, this.showSelectedBold,
+    this.alwaysOpened,
+    this.showSelectedBold,
     // this.appBar,
   }) : super(key: key);
 
@@ -341,7 +340,7 @@ class SideBarScaffold extends StatefulWidget {
 
 class _SideBarScaffoldState extends State<SideBarScaffold>
     with SingleTickerProviderStateMixin {
-  PageController _pageController;
+  PageController? _pageController;
 
   List<int> _history = [];
 
@@ -354,26 +353,24 @@ class _SideBarScaffoldState extends State<SideBarScaffold>
 
   @override
   void dispose() {
-    _pageController.dispose();
+    _pageController!.dispose();
     super.dispose();
   }
 
   void show(int index) {
-    _pageController.jumpToPage(index);
+    _pageController!.jumpToPage(index);
     _history.add(index);
     _history.toSet().toList();
   }
 
-
   Future<bool> _onBackPressed() async {
-    if(_history.length == 1){
+    if (_history.length == 1) {
       return false;
     }
     _history.removeLast();
-    _pageController.jumpToPage(_history.last);
+    _pageController!.jumpToPage(_history.last);
     return false;
   }
-
 
   @override
   Widget build(BuildContext context) {
